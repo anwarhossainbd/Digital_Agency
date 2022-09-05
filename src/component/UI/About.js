7975b27974a1a6af2351f5c57e3c758016d00f86
anwarhossainbd <mdanwarhossain1996bangladesh@gmail.com>
2022-09-05 16:49:43 +0600

    About Part done

--- src/component/UI/About.js
+++ src/component/UI/About.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import "../../styles/about.css"
+import "../../styles/about.css";
+import aboutImg from "../../images/about-us.jpg"
 
 const chooseData =[
 
     {
         icon:'ri-wifi-line',
         title:'First working process',
-        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.  '
     },
 
 
     {
-        icon:'ri-wifi-line',
-        title:'First working process',
-        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'
+        icon:'ri-team-line',
+        title:'Dedicated team',
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
     },
 
     {
-        icon:'ri-wifi-line',
-        title:'First working process',
-        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,'
+        icon:'ri-customer-service-2-line',
+        title:'24/7 Hours support',
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
     },
 
 
@@ -36,28 +37,39 @@ const About = () => {
               <h6 className="subtitle"> Why choose us</h6>
               <h2 className="highlight">financial challenges</h2>
               <p className="description about__content-desc">
-                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took,
               </p>
 
-              <div className="choose__us-item">
+            
+              <div className="choose__item-wrapper">
+              {
+                chooseData.map((item,index)=>(
+
+                      <div className="choose__us-item">
                 <span className="choose__us-icon">
-                   <i class="ri-wifi-line"></i>
+                   <i class={item.icon}></i>
                 </span>
 
                 <div>
-                    <h4 className="choose__us-title">First working process</h4>
+                    <h4 className="choose__us-title">{item.title}</h4>
                     <p className="description">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-
+                       {item.desc}
                     </p>
                 </div>
+              
+              </div>
+
+                ))
+              }
               
               </div>
 
 
            </div>
 
-           <div className="about__img"></div>
+           <div className="about__img">
+              <img src={aboutImg} alt="" />
+           </div>
 
         </div>
       </div>
